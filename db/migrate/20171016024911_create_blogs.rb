2c7a7594cc609fdcd8054c257342c61d3dfb8471
Prelang Builder <builder@prelang.com>
2017-10-16 02:49:12 +0000

    Generated model Blog with migration

---
+++ db/migrate/20171016024911_create_blogs.rb
@@ -0,0 +1,10 @@
+class CreateBlogs < ActiveRecord::Migration
+  def change
+    create_table :blogs do |t|
+      t.references :user, index: true
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
